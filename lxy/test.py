3fc41e1c575cf90e47e5a9dde198da4829f3b2f3
LeeSherry02 <2869036826@qq.com>
2025-02-20 14:15:13 +0000

    终止所有进程python代码（除了发起该命令的进程）

--- lxy/test.py
+++ lxy/test.py
@@ -1,5 +1,4 @@
-import torch
-print(torch.cuda.current_device())
-print(torch.cuda.get_device_name(0))
-print(torch.__version__)
-print(torch.version.cuda)
+import subprocess
+
+# 终止所有进程（除了发起该命令的进程）
+subprocess.run(['pkill', '-9', '-f', 'python'])
