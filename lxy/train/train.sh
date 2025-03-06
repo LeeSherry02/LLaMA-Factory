634ea5d5c71f2b68b547cb126639ce9465c879fb
LeeSherry02 <2869036826@qq.com>
2025-03-06 09:23:28 +0000

    1、注册数据集：MedThoughts-8K 2、train.sh添加可自定义的：cutoff_len 主要解决long cot数据需要修改cutoff_len的问题（不能太短）

--- lxy/train/train.sh
+++ lxy/train/train.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-DATASET='huatuo_26m_lite_score_5_alpaca_3*500'
+DATASET='MedThoughts-8K'
 PER_DEVICE_TRAIN_BATCH_SIZE=1
-GRADIENT_ACCUMULATION_STEPS=32
+GRADIENT_ACCUMULATION_STEPS=16
+NUM_TRAIN_EPOCHS=5.0  # 添加 num_train_epochs 变量
+LEARNING_RATE=1.0e-6  # 添加 learning_rate 变量
+CUTOFF_LEN=2048
 
 # wandb信息
 WANDB_PROJECT="qwen_full_sft"
-WANDB_NAME="${DATASET}"
+WANDB_NAME="${DATASET}_${LEARNING_RATE}_${CUTOFF_LEN}"
 
 # 修改yaml文件配置
 YAML_FILE="/remote-home/xiaoyili/2025-Medical/LLaMA-Factory/lxy/train/qwen_full_sft_wandb.yaml"
 MODEL_NAME_OR_PATH='/remote-home/xiaoyili/2025-Medical/model/Qwen/Qwen2___5-1___5B'
-OUTPUT_DIR="/remote-home/xiaoyili/2025-Medical/LLaMA-Factory/lxy/saves/qwen2.5-1.5b/${DATASET}"
+OUTPUT_DIR="/remote-home/xiaoyili/2025-Medical/LLaMA-Factory/lxy/saves/qwen2.5-1.5b/${DATASET}/${LEARNING_RATE}/${CUTOFF_LEN}"
 
 # 备份文件名称
 YAML_FILE_BAK="${YAML_FILE}.bak"
@@ -37,6 +40,9 @@ sed -i "s|output_dir: .*|output_dir: ${OUTPUT_DIR}|" "${YAML_FILE}"
 sed -i "s|per_device_train_batch_size: .*|per_device_train_batch_size: ${PER_DEVICE_TRAIN_BATCH_SIZE}|" "${YAML_FILE}"
 sed -i "s|gradient_accumulation_steps: .*|gradient_accumulation_steps: ${GRADIENT_ACCUMULATION_STEPS}|" "${YAML_FILE}"
 sed -i "s|run_name: .*|run_name: ${WANDB_NAME}|" "${YAML_FILE}"
+sed -i "s|num_train_epochs: .*|num_train_epochs: ${NUM_TRAIN_EPOCHS}|" "${YAML_FILE}"  # 添加 num_train_epochs 替换
+sed -i "s|learning_rate: .*|learning_rate: ${LEARNING_RATE}|" "${YAML_FILE}"  # 添加 learning_rate 替换
+sed -i "s|cutoff_len: .*|cutoff_len: ${CUTOFF_LEN}|" "${YAML_FILE}"  # 添加 cutoff_len 替换
 
 # 3、运行训练命令
 llamafactory-cli train "${YAML_FILE}"
